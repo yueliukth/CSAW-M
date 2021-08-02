de03ac3979b9e4ddbaa9f94e0cae49b722dc9fbb
yueliukth <yue3@kth.se>
2021-08-02 22:41:54 +0200

    Updated preprocessing script to move centroid of the breast to the image center along x-direction

--- src/data_handler/preprocessing.py
+++ src/data_handler/preprocessing.py
@@ -11,6 +11,49 @@ import traceback
 
 import helper
 
+def get_breast_percent(img_array, if_pad_small_resolution=False, if_large_resolution=False):
+    # pad small resolution images if necessary
+    if if_pad_small_resolution:
+        if if_large_resolution:
+            x_max = 3328
+            y_max = 4096
+        else:
+            x_max = 512
+            y_max = 632
+        margin_x = int((x_max - img_array.shape[1]) / 2)
+        margin_y = int((y_max - img_array.shape[0]) / 2)
+        if margin_x != 0 or margin_y != 0:
+            img_array = cv2.copyMakeBorder(src=img_array, top=margin_y, bottom=margin_y, left=margin_x, right=margin_x,
+                                           borderType=cv2.BORDER_CONSTANT, value=0)
+
+    # segment the breast
+    img_breast_only, breast_mask, _ = segment_breast(img_array)
+
+    # calculate the breast area, whole image area and breast percent area
+    mask_area = np.count_nonzero(breast_mask)
+    img_area = breast_mask.shape[0] * breast_mask.shape[1]
+    breast_percent = mask_area / img_area
+    return breast_percent
+
+def threshold_img(img, low_int_threshold=0.05):
+    # create img for thresholding and contours
+    img_8u = (img.astype('float32') / img.max() * 255).astype('uint8')
+
+    if low_int_threshold < 1:
+        low_th = int(img_8u.max() * low_int_threshold)
+    else:
+        low_th = int(low_int_threshold)
+    _, img_bin = cv2.threshold(img_8u, low_th, maxval=255, type=cv2.THRESH_BINARY)
+    return img_bin
+
+def get_centroid(img, low_int_threshold=0.05):
+    _, _, biggest_contour = segment_breast(img, low_int_threshold)
+
+    M = cv2.moments(biggest_contour)
+
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return cx, cy
 
 def get_contours(array, min_contour_area=10, kernel_size=(10, 10), save_as=None, colorful=False):
     array_max = 2 ** 16 - 1  # assumption: image read as 16-bit values
@@ -89,34 +132,20 @@ def get_contours(array, min_contour_area=10, kernel_size=(10, 10), save_as=None,
         return contours  # list being either empty or containing one contour
 
 
-# function to remove texts in the image
-# only breast remained (note: this function is not used when removing the text)
-def segment_breast(img, low_int_threshold=0.05, only_breast_bbox=True):
-    # create img for thresholding and contours
-    img_8u = (img.astype('float32') / img.max() * 255).astype('uint8')
-    if low_int_threshold < 1:
-        low_th = int(img_8u.max() * low_int_threshold)
-    else:
-        low_th = int(low_int_threshold)
-    _, img_bin = cv2.threshold(img_8u, low_th, maxval=255, type=cv2.THRESH_BINARY)
-
-    _, contours, _ = cv2.findContours(img_bin.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def segment_breast(img, low_int_threshold=0.05):
+    img_bin = threshold_img(img, low_int_threshold)
+    # find the largest contour
+    contours, _ = cv2.findContours(img_bin.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cont_areas = [cv2.contourArea(cont) for cont in contours]
     idx = np.argmax(cont_areas)
+    biggest_contour = contours[idx]
+
     breast_mask = cv2.drawContours(np.zeros_like(img_bin), contours, idx, 255, -1)
-    
+
     # segment the breast
     img_breast_only = cv2.bitwise_and(img, img, mask=breast_mask)
 
-    # the case were we want the whole image (with breast segmented and everywhere else 0's) to be returned
-    if not only_breast_bbox:
-        return img_breast_only, None
-
-    # otherwise return only the bounding box around the segmented breast
-    x, y, w, h = cv2.boundingRect(contours[idx])
-    img_breast_only = img_breast_only[y:y+h, x:x+w]
-    return img_breast_only, (x, y, w, h)
-
+    return img_breast_only, breast_mask, biggest_contour
 
 # function to locate the center of mass
 def new_cropping_single_dist(img):
@@ -190,7 +219,6 @@ def pad_or_crop_single_maxloc(img, maxloc, full_height=632, full_width=512, if_m
             int((img_new.shape[1] - img.shape[1]) / 2):int((img_new.shape[1] - img.shape[1]) / 2) + img.shape[1]] = img
     return img_new
 
-
 def raw_to_preprocessed(image_folder, labels_path, save_dir, if_movey=True):
     # read the csv file as df
     df = pd.read_csv(labels_path, delimiter=';', dtype={'sourcefile': str})
@@ -206,7 +234,7 @@ def raw_to_preprocessed(image_folder, labels_path, save_dir, if_movey=True):
         dicom_windowcenter = row['Dicom_window_center']
         dicom_windowwidth = row['Dicom_window_width']
         dicom_photometricinterpretation = row['Dicom_photometric_interpretation']
-        
+
         img_path = os.path.join(image_folder, filename)
         img_array = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)  # read the 16-bit PNG as is
 
@@ -218,15 +246,20 @@ def raw_to_preprocessed(image_folder, labels_path, save_dir, if_movey=True):
         # note: CSAW-M images have one value for dicom_windowcenter and dicom_windowwidth (which may not be the case in other datasets)
         img_array = exposure.rescale_intensity(img_array, in_range=(dicom_windowcenter - dicom_windowwidth / 2,
                                                                     dicom_windowcenter + dicom_windowwidth / 2))
+
+        # convert 16 bits to 8 bits
+        img_array = (img_array / 256).astype(np.uint8)
         min_val = int(np.min(img_array))  # take note of the min values in the rescaled array
 
         # invert the color if needed
         if dicom_photometricinterpretation == 'MONOCHROME1':
             img_array = cv2.bitwise_not(img_array)
 
-        # crop with distance transform and pad
-        max_loc = new_cropping_single_dist(img_array)
-        new_img = pad_or_crop_single_maxloc(img_array, max_loc, if_movey=if_movey)
+        # get the centroid of the breast
+        cx, cy = get_centroid(img_array)
+
+        # move the centroid of the breast to the center of the image along x-axis only
+        new_img = pad_or_crop_single_maxloc(img_array, (cx, cy), if_movey=if_movey)
 
         contours_list = get_contours(new_img)
         print(f'contour: {contours_list}')
@@ -238,54 +271,10 @@ def raw_to_preprocessed(image_folder, labels_path, save_dir, if_movey=True):
         if not os.path.exists(os.path.dirname(new_filepath)):
             os.makedirs(os.path.dirname(new_filepath))
             print(f'Created folder: {os.path.dirname(new_filepath)}')
-            
-        cv2.imwrite(new_filepath, new_img.astype(np.uint16))
+
+        cv2.imwrite(new_filepath, new_img.astype(np.uint8))
         print(f'Image saved to: \n{new_filepath}')
         print(f'Done for image {i + 1}/{df.shape[0]}\n')
-
-
-def _save_as_png(dicom_folder, dicom_basenames, png_folder, image_size, reduce_bits=False):
-    """
-    Notes:
-        - image_size should either be a (w, h) tuple like (512, 632) or a dict like {(3328, 4096): (512, 632), (2560, 3328): (394, 514)}.
-    """
-    assert type(image_size) == tuple or type(image_size) == dict, 'image_size should either be a tuple or a dict'
-
-    for i, dicom_basename in enumerate(dicom_basenames):
-        dicom_filepath = os.path.join(dicom_folder, dicom_basename)
-        png_basename = dicom_basename.replace('.dcm', '.png')
-        new_path = os.path.join(png_folder, png_basename)
-
-        try:
-            img_dcm = pydicom.dcmread(dicom_filepath)
-            img_array = img_dcm.pixel_array
-        except:
-            message = traceback.format_exc()  # get full traceback
-            print(f'A problem occurred when reading: {dicom_filepath}: \n{message}')
-            exit(1)
-
-        # determine down-sampled image size to keep the aspect ratio of the original resolution images
-        if type(image_size) == tuple:
-            determined_image_size = image_size
-        elif type(image_size) == dict:
-            orig_shape = img_array.shape  # (h, w)
-            orig_shape_wh = (img_array.shape[1], img_array.shape[0])  # (w, h)
-            assert orig_shape_wh in image_size, f'Down-sampling size for orig_shape={orig_shape_wh} not specified in image_size={image_size}'
-            determined_image_size = image_size[orig_shape_wh]
-        else:
-            raise NotImplementedError('"determined_image_size" cannot be calculated for the current image_size')
-
-        # save images
-        if reduce_bits:  # this reduces the quality of the PNG image - it has not been used in the project
-            img_array = img_array / (2 ** 16 - 1)
-            img_array = helper.unnormalize_image(img_array)  # reduce to 8 bits, using np.astype(np.uint8) destroys the image
-            Image.fromarray(img_array).convert('RGB').resize(determined_image_size).save(new_path)  # save as RGB not gray
-        else:
-            cv2.imwrite(new_path, cv2.resize(img_array.astype(np.uint16), determined_image_size))
-
-        print(f'Image saved to: \n{new_path}')
-        print(f'Done for image {i + 1}/{len(dicom_basenames)}\n')
-
 
 def dicoms_to_raw_pngs(dicom_folder, dicom_basenames, png_folder, image_size, reduce_bits=False, n_processes=1):
     # this is the main functions that is used to preprocess all images to png (supports multi-processing)
@@ -300,47 +289,3 @@ def dicoms_to_raw_pngs(dicom_folder, dicom_basenames, png_folder, image_size, re
             pool.starmap(_save_as_png, all_args)
     else:
         _save_as_png(dicom_folder, dicom_basenames, png_folder, image_size, reduce_bits)
-
-# given dicom_path that points to dicom images, this function outputs a csv file with all image basenames, full paths, and other dicom attributes that should be considered while preprocessing
-def get_dicom_attr_from_dcm(dicom_path): 
-    df = pd.DataFrame()
-    i = 0
-    for root, fodler, files in os.walk(dicom_path):
-        for name in files:
-            if name.endswith('.dcm'):        
-                img_path = os.path.join(root,name)
-                img_dcm = pydicom.dcmread(img_path)
-                img_array = img_dcm.pixel_array
-                dicom_windowcenter = img_dcm.WindowCenter
-                dicom_windowwidth= img_dcm.WindowWidth
-                dicom_imagelaterality = img_dcm.ImageLaterality
-                dicom_photometricinterpretation = img_dcm.PhotometricInterpretation
-                dicom_bitsallocated = img_dcm.BitsAllocated
-                dicom_bitsstored = img_dcm.BitsStored
-                try:
-                    dicom_voilutfunction = img_dcm.VOILUTFunction
-                except Exception as e:
-                    dicom_voilutfunction = ''
-                try:
-                    dicom_imagerpixelspacing = str(img_dcm.ImagerPixelSpacing)
-                except Exception as e:
-                    dicom_imagerpixelspacing = ''
-                try:
-                    dicom_pixelspacing = str(img_dcm.PixelSpacing)
-                except Exception as e:
-                    dicom_pixelspacing = ''
-                df.loc[i,'dicom_basename']=name
-                df.loc[i,'dicom_path']=img_path
-                df.loc[i,'dicom_resolution']=str(np.shape(img_array))
-                df.loc[i,'dicom_bitsallocated']=dicom_bitsallocated
-                df.loc[i,'dicom_bitsstored']=dicom_bitsstored
-                df.loc[i,'dicom_imagerpixelspacing']=dicom_imagerpixelspacing
-                df.loc[i,'dicom_pixelspacing']=dicom_pixelspacing                
-                df.loc[i,'dicom_windowcenter']=dicom_windowcenter
-                df.loc[i,'dicom_windowwidth']=dicom_windowwidth
-                df.loc[i,'dicom_voilutfunction']=dicom_voilutfunction
-                df.loc[i,'dicom_imagelaterality']=dicom_imagelaterality
-                df.loc[i,'dicom_photometricinterpretation']=dicom_photometricinterpretation
-                i += 1
-    return df
-
